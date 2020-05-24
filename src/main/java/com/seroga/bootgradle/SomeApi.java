431834553cf6b7bbb1623ec32c9d86b962bf25de
Sergei Kossik <fretfan92@gmail.com>
2020-05-24 12:47:26 +0300

    base

---
+++ src/main/java/com/seroga/bootgradle/SomeApi.java
@@ -0,0 +1,13 @@
+package com.seroga.bootgradle;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SomeApi {
+
+  @GetMapping("/1")
+  public String one() {
+    return "I am working!";
+  }
+}
---
+++ src/test/java/com/seroga/bootgradle/BootGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seroga.bootgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
