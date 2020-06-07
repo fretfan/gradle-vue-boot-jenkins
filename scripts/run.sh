c8c919f6e6f88a840348e59f32ef3cc3bdf86641
Sergei Kossik <sergei.kossik@ignite.ee>
2020-06-07 23:51:52 +0300

    fix run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #cat app.pid
 #echo 'killing if pid if running'
+export BUILD_ID=dontKillMe #otherwise jenkins kills java -jar, when build finishes
 
 rm -fv nohup.out
-export BUILD_ID=dontKillMe && nohup > nohup.out java -jar ../build/libs/gradle-vue-boot-jenkins-0.0.1-SNAPSHOT.war &
+nohup > nohup.out java -jar ../build/libs/gradle-vue-boot-jenkins-0.0.1-SNAPSHOT.war &
 app_pid=$!
 echo $app_pid > app.pid
 echo "starting app with PID: '$app_pid'"
